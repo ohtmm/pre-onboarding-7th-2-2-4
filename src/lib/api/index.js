5da0f039252b05a2827a95fef17f31e0d29ddab9
ganeodolu <ganeodolu@gmail.com>
2022-11-05 23:22:32 +0900

    feat: mockup 데이터 api 구현

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,10 +1,13 @@
-const delayFetch = (url, options) =>
+import { BASE_URL } from "../constants";
+
+const delayFetch = (filename, delayTime) =>
   new Promise((resolve) => {
     setTimeout(() => {
-      resolve(fetch(url, options));
-    }, options.delay);
+      resolve(fetch(BASE_URL + filename));
+    }, delayTime);
   });
 
-const api = (url, delay) => delayFetch(url, { delay });
+const api = (filename, delayTime = 100) =>
+  delayFetch(filename, delayTime).then((response) => response.json());
 
 export default api;
---
+++ src/lib/constants/index.js
@@ -0,0 +1,6 @@
+export const BASE_URL = process.env.PUBLIC_URL + "assets/resources/";
+
+export const FILENAME = {
+  TREND_DATA_SET: "wanted_FE_trend-data-set.json",
+  AD_LIST_DATA_SET: "wanted_FE_ad-list-data-set.json"
+};
